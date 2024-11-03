8510c9b7f07714b2da2c45caee1011c278b9e4b2
Somasekhar <sunkarisekhar36@gmail.com>
2024-11-03 16:52:52 +0530

    Docker Private repo secret Added

---
+++ privaterepos/steps.sh
@@ -0,0 +1,1 @@
+# kubectl create secret docker-registry dockercred --docker-server="https://index.docker.io/v1/" --docker-username="sunkarisomasekhar" --docker-password="" --docker-email="sunkarisekhar36@gmail.com"
